1a0895926e08d9a3ffb399690d461c314ac2c24a
Puneet <40600761+Puniit00@users.noreply.github.com>
2022-10-14 12:45:28 +0530

    Added solution - LeetHub

---
+++ Delete middle element of a stack - GFG/delete-middle-element-of-a-stack.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        
+        while(t-->0)
+        {
+            int sizeOfStack =sc.nextInt();
+            Stack <Integer> myStack=new Stack<>();
+            
+            //adding elements to the stack
+            for(int i=0;i<sizeOfStack;i++)
+            {
+                int x=sc.nextInt();
+                myStack.push(x);
+                
+            }
+                Solution obj=new Solution();
+                obj.deleteMid(myStack,sizeOfStack);
+                
+                while(!myStack.isEmpty())
+                {
+                    System.out.print(myStack.peek()+" ");
+                    myStack.pop();
+                }
+                System.out.println();
+        }
+        
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer>s,int sizeOfStack){
+        delete(s, 0, sizeOfStack);
+    } 
+    private void delete(Stack<Integer>s, int i, int n){
+        if(n == i) return;
+        int top = s.pop();
+        delete(s, i+1, n);
+         if(i == n/2) return;
+         else {
+             s.push(top);
+             return;
+         }
+    }
+}
+
