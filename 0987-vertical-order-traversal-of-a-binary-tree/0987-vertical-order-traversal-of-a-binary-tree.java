c2587eda873a43b2ff3faddc36f4937d5ca8557a
Puneet <40600761+Puniit00@users.noreply.github.com>
2022-10-19 10:50:52 +0530

    Time: 5 ms (65.06%), Space: 44 MB (6.66%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        
+        vertical(root, map, 0, 0); // traverse in tree and fill the map 
+        
+		List<List<Integer>> res = new ArrayList<>();
+        for(TreeMap<Integer, PriorityQueue<Integer>> vlevels: map.values()){
+            
+            res.add(new ArrayList<>()); // add a new list
+            
+            for(PriorityQueue<Integer> hlevels: vlevels.values()){
+                
+                while(!hlevels.isEmpty()){
+                    res.get(res.size()-1).add(hlevels.poll());
+                }
+            }
+        }
+        return res;
+    }
+    
+    void vertical(TreeNode root, TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map, int vlevel, int hlevel){
+        
+        if(root == null) return;
+
+        if(!map.containsKey(vlevel)) map.put(vlevel, new TreeMap<>());
+        
+        if(!map.get(vlevel).containsKey(hlevel)) 
+            map.get(vlevel).put(hlevel, new PriorityQueue<>());
+
+        map.get(vlevel).get(hlevel).add(root.val);
+
+        vertical(root.left, map, vlevel-1, hlevel+1);
+        
+        vertical(root.right, map, vlevel+1, hlevel+1);
+        
+    }
+    
+}
