42df4ad0d3e9f992c969f5a6642274595d684d2e
Puneet <40600761+Puniit00@users.noreply.github.com>
2022-11-13 14:42:46 +0530

    Added solution - LeetHub

--- Minimum Cost To Make Two Strings Identical - GFG/minimum-cost-to-make-two-strings-identical.java
+++ Minimum Cost To Make Two Strings Identical - GFG/minimum-cost-to-make-two-strings-identical.java
@@ -28,27 +28,24 @@ class GfG
 //User function Template for Java
 
 class Solution
-{   int[][] dp = new int[1001][1001];
+{ 
 	public int findMinCost(String X, String Y, int costX, int costY)
 	{
-	    for(int i=0;i<X.length(); i++){
-	        for(int j=0;j<Y.length();j++){
-	            dp[i][j] = -1;
+	    int[][] dp = new int[X.length()+1][Y.length()+1];
+	    for(int i=1;i<=X.length(); i++){
+	        for(int j=1;j<=Y.length();j++){
+	             if(X.charAt(i-1) == Y.charAt(j-1)){
+	                 dp[i][j] = 1+dp[i-1][j-1];
+	             }
+	             else{
+	                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); 
+	             }
 	        }
 	    }
-		int lcs = lcs(X, Y, 0, 0);
+		int lcs = dp[X.length()][Y.length()];
 		int cost1 = (X.length()- lcs)*costX;
 		int cost2 = (Y.length()-lcs)*costY;
 		
 		return cost1+cost2;
-	}
-	
-	private int lcs(String x, String y, int i, int j){
-	    if(i==x.length() || j == y.length()) return 0;
-	    
-	    if(dp[i][j] != -1) return dp[i][j];
-	    if(x.charAt(i) == y.charAt(j)) return dp[i][j] = 1+lcs(x, y, i+1, j+1);
-	    
-	    return dp[i][j] = Math.max(lcs(x, y, i+1, j), lcs(x, y, i, j+1));
 	}
 }
